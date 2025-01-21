4dd0fdccf28a1a52397b7fe4b98ee12be4f9e004
Nada Yasser <nadayasser358@gmail.com>
2025-01-21 13:32:47 +0200

    navigation between taps logic

---
+++ lib/common/app_images.dart
@@ -0,0 +1,16 @@
+class AppImages{
+  static const String lightBg="assets/images/default_bg.png";
+  static const String darkBg="assets/images/dark_bg.png";
+  static const String quranIcon="assets/images/icon_quran.png";
+  static const String hadithIcon="assets/images/icon_hadeth.png";
+  static const String sebhaIcon="assets/images/icon_sebha.png";
+  static const String radioIcon="assets/images/icon_radio.png";
+  static const String quranLogo="assets/images/qur2an_screen_logo.png";
+  static const String hadithLogo="assets/images/hadeth_logo.png";
+  static const String sebhaHeadLight="assets/images/head_sebha_logo.png";
+  static const String sebhaBodyLight="assets/images/body_sebha_logo.png";
+  static const String sebhaHeadDark="assets/images/head_sebha_dark.png";
+  static const String sebhaBodyDark="assets/images/body_sebha_dark.png";
+  static const String radioLogo="assets/images/radio_image.png";
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/ui/home.dart';
+
+import 'common/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.light,
+      routes: {
+        HomeScreen.routeName: (_)=>HomeScreen()
+      },
+      initialRoute: HomeScreen.routeName,
     );
   }
 }
